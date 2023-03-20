03d4837cd42a4f07b063cfa2c0243e7f3d26c2cc
Krishna Kumar <nnkrishna714@gmail.com>
2023-03-20 15:05:49 +0530

    commit

---
+++ comment.dart
@@ -0,0 +1,36 @@
+// Comment:
+  // -> Dart support three type of comment syntex.
+  //   1-> single line comment --> to test our code , to create notes
+  //   2-> Mutiline comment 
+  //   3-> Documentation type comment 
+void main(){
+  /*
+  this is krishna
+  and I am AI trainer 
+  studing in MCA final year
+  
+  Looking for phd in australia */
+  print("hello world");   //print() function is use to print/echo something
+  print("hello world");
+  print("hello world");
+  print("hello world");
+  print("hello world");
+  print("hello world");
+  print("hello world");
+  print("hello world");
+  print("hello world");   
+  krishna();
+}
+
+void krishna(){
+  print("this is my new function");
+}
+
+
+/**java convention
+ *  this is krishna
+ * 
+ */
+
+///C# convention 
+///this is krishna
---
+++ variable.dart
@@ -0,0 +1,29 @@
+// Variable:
+// > Variable is a container also known as identifier.
+// > Variable name never start with number 
+// > Variable doesnot contain special character except underscore("_") and $
+// > Variable must be readable
+
+void main(){
+  var krishna=10;
+  print("krishna");
+  print(krishna);
+  var a1krishna=20;
+  print(a1krishna);
+  var $k=30;
+  print($k);
+  
+  var k$=40;
+  print(k$);
+
+  var krishna_age=27;
+  var krishna_weight=74;
+
+  print(krishna_age);
+  print(krishna_weight);
+  
+  var varonikaRodrigs=10;   //camel case variable name
+  var VaronikaRodrigs=10;   //Pascal case variable name
+  var varonika_rodrigs=10;  //Snake case variable name 
+  var Varonika&Rodrigs=10;
+}
